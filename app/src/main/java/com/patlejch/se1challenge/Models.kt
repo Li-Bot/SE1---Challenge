9bb82446fe2c86bb8bf9d2a8ab0e162bd3715d8e
Karel Patlejch <patlejch.karel@gmail.com>
2021-08-14 22:02:13 +0100

    added and reorganised provided code

---
+++ app/src/main/java/com/patlejch/se1challenge/Models.kt
@@ -0,0 +1,9 @@
+package com.patlejch.se1challenge
+
+data class Order(val id: String)
+
+data class OrderPrice(val price: Double)
+
+data class PriceMultiplier(val value: Double)
+
+data class PriceDiscount(val value: Double)
